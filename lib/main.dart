130de985065c553b98a1cce4d9f7df7cd42ab6b4
arpon-jolas-santos <jolasarpon1@gmail.com>
2025-03-27 18:31:05 +0800

    commit by jolas

--- lib/main.dart
+++ lib/main.dart
@@ -47,3 +47,35 @@ class _HomepageState extends State<Homepage> {
     print(response.body);
     getData(); // Refresh the user list after deletion
   }
+   @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: SafeArea(
+        child: ListView.builder(
+          itemCount: user.length,
+          itemBuilder: (context, int index) {
+            final item = user[index];
+            TextEditingController _password =
+            TextEditingController(text: item['password']);
+
+            return CupertinoListTile(
+              title: Text(item['username']),
+              trailing: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CupertinoButton(
+                    child: Icon(CupertinoIcons.trash_fill,
+                        color: CupertinoColors.destructiveRed),
+                    onPressed: () {
+                      showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            title: Text(
+
