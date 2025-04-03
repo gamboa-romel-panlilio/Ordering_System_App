c022f6367248f52811a0edc765f8c8d614a8db00
gamboa-romel-panlilio <melgrowtopia029@gmail.com>
2025-04-03 20:20:05 +0800

    commit by romel

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ordering_system_app/main.dart';
+import 'package:QuickCart/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
